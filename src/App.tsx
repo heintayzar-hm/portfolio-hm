d4a38b2f46eaaf787ad94050dfc74d855d7bea42
Hein Tay Zar <heintayzarhm@gmail.com>
2023-01-19 21:37:08 +0630

    Install

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-900 hover:bg-black">Portfolio</h1>
+      <img
+        src="https://images.unsplash.com/photo-1510936111840-65e151ad71bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1490&q=80"
+        alt="bg"
+      />
+    </>
+  );
+}
+
+export default App;
